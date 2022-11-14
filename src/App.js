61dec3e36a460ca832f1188297ecf384036f5f99
Farhad Abbasi <farhad.abbasi9@gmail.com>
2022-11-14 22:40:18 +0330

    14-dynamic-data

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends React.Component{
                 <Product />
                 <Product />
                 <Product />
-                <Product />
             </div>
         );
     }
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,5 +1,12 @@
 const Product = () => {
-   return  <div>Product Name: Book</div>
+    const price = 89;
+
+   return (
+    <div>
+        <div>Product Name: Book</div>
+        <div>Product Price: {price}</div>
+    </div>
+  )
 }
 
 export default Product;
